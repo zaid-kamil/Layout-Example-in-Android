be0fb2387c93d1b9691252bd7daff64a8f75d0d3
zaid kamil <xaidmetamorphos@gmail.com>
2021-11-03 18:35:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/layoutapp1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layoutapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.oyster_layout_1)
+    }
+}
